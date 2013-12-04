d120bd302004fc748a897a58bf1137df3685f791
vanquished <predictionsofyesterday@gmail.com>
2013-12-03 20:23:01 -0700

    some basic updates + graphics

--- operation-3067.py
+++ operation-3067.py
@@ -3,10 +3,37 @@
 #Written by vanquished
 #Licenced under the GPL v3 licence.
 
-#uses
-import pygame
+VERSION = "0.1"
 
-#vars
+try:
+    import pygame, sys
+    from pygame.locals import *
+except ImportError, err:
+    print "Could not load module. {0}".format(err)
 
-
-#begin
+def main():
+    #Initialise screen
+    pygame.init()
+    screen = pygame.display.set_mode((800, 540))
+    pygame.display.set_caption("Operation 3067 version "+ VERSION)
+    
+    #Background
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((1, 136, 186))
+    # Rest of code down here
+    screen.blit(background, (0,0))
+    pygame.display.flip()
+    #The main loop
+    while True:
+        #events
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        pygame.display.update()
+        #state
+        #draw on screen
+        
+if __name__ == "__main__":
+    main()
